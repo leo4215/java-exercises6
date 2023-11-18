1705ee8bf6389958faf7a5ed4946512a617cc72c
Leonardo de Camargo <134113848+leo4215@users.noreply.github.com>
2023-11-18 18:54:57 -0500

    Create Ex6.java

---
+++ Ex6.java
@@ -0,0 +1,30 @@
+/* Faça um programa que leia N números reais e armazene-os em um vetor. Em seguida, mostrar na tela o maior número do vetor (supor não haver empates). Mostrar também a posição do maior elemento, considerando a primeira posição como 0 (zero). */
+
+import java.util.Scanner;
+
+public class Ex6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("How many number are you digiting? ");
+        int index = 0, n = sc.nextInt();
+
+        double[] vect = new double[n];
+
+        for (int i = 0; i < vect.length; i++){
+            System.out.print("Digit a number: ");
+            vect[i] = sc.nextDouble();
+        }
+
+        double maxValue = vect[0];
+        for (int i = 0; i < vect.length; i++) {
+            if(vect[i] > maxValue){
+                maxValue = vect[i];
+                index = i;
+            }
+        }
+
+        System.out.printf("\nHIGHEST VALUE = %.2f\n", maxValue);
+        System.out.printf("HIGHEST VALUE POSITION = %d", index);
+        sc.close();
+    }
+}
