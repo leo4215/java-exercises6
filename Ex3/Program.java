b99603fcffa99282c7ebd55fe85529444d099f67
Leonardo de Camargo <134113848+leo4215@users.noreply.github.com>
2023-11-18 16:19:00 -0500

    Create Program.java

---
+++ Ex3/Program.java
@@ -0,0 +1,26 @@
+/* Faça um programa que leia um número inteiro positivo N (máximo = 10) e depois N números inteiros e armazene-os em um vetor. Em seguida, mostrar na tela todos os números negativos lidos. */
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("How many numbers are you entering? ");
+        int n = sc.nextInt();
+
+        int[] vect = new int[n];
+
+        for (int i = 0; i < vect.length; i++){
+            System.out.print("Enter a number: ");
+            vect[i] = sc.nextInt();
+        }
+
+        System.out.println("NEGATIVE NUMBERS:");
+        for (int i = 0; i <vect.length; i++){
+            if (vect[i] < 0){
+                System.out.printf("%d\n", vect[i]);
+            }
+        }
+        sc.close();
+    }
+}
