50e5626e320aab9e84241c70b3c8e9cb77c10048
Leonardo de Camargo <134113848+leo4215@users.noreply.github.com>
2023-11-19 10:54:27 -0500

    Create People.java

---
+++ Ex11/People.java
@@ -0,0 +1,26 @@
+public class People {
+    
+    private String name;
+    private int age;
+
+    public People(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public int getAge(){
+        return age;
+    }
+}
