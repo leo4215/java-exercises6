d65e52827bc7f929deb58b606ef455ebad326fb2
Leonardo de Camargo <134113848+leo4215@users.noreply.github.com>
2023-11-19 17:06:12 -0500

    Create Program.java

---
+++ Ex12/Program.java
@@ -0,0 +1,31 @@
+/* Fazer um programa para ler um conjunto de N nomes de alunos, bem como as notas que eles tiraram no 1º e 2º semestres. Cada uma dessas informações deve ser armazenada em um vetor. Depois, imprimir os nomes dos alunos aprovados, considerando aprovados aqueles cuja média das notas seja maior ou igual a 6.0 (seis). */
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("How many students are you entering? ");
+        int n = sc.nextInt();
+
+        Student[] vect = new Student[n];
+
+        for (int i = 0; i < vect.length; i++){
+            sc.nextLine();
+            System.out.printf("Enter the name, the 1st and the 2nd grade of student %d:\n", i + 1);
+            String name = sc.nextLine();
+            double grade1 = sc.nextDouble();
+            double grade2 = sc.nextDouble();
+            vect[i] = new Student(name, grade1, grade2);
+        }
+        
+        System.out.println("\nStudents that passed:");
+        for (int i = 0; i < vect.length; i++){
+            double finalGrade = (vect[i].getGrade1() + vect[i].getGrade2()) / 2;
+            if (finalGrade >= 6){
+                System.out.printf("%s\n", vect[i].getName());
+            } 
+        }
+        sc.close();
+    }
+}
