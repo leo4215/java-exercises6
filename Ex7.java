b0f24ca56f1425b3dcf282462227c2c700a2b009
Leonardo de Camargo <134113848+leo4215@users.noreply.github.com>
2023-11-19 09:44:27 -0500

    Create Ex7.java

--- Ex7.java
+++ Ex7.java
@@ -2,7 +2,7 @@
 
 import java.util.Scanner;
 
-public class Ex6 {
+public class Ex7 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("How many number are you digiting? ");
