32e0909edc56112131a5c9a7d58658a04c83a2f4
Leonardo de Camargo <134113848+leo4215@users.noreply.github.com>
2024-01-02 22:54:54 -0500

    Update Ex1.java

--- Ex1.java
+++ Ex1.java
@@ -5,19 +5,15 @@ import java.util.Scanner;
 public class Ex1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int n = sc.nextInt();
-        double[] vect = new double[n];
 
+        double[] vect = new double[n];
+        
+        double sum = 0;
         for (int i = 0; i < n; i++) {
             vect[i] = sc.nextDouble();
-        }
-
-        double sum = 0.0;
-        for (int i = 0; i < n; i++) {
             sum += vect[i];
         }
-
         double avg = sum / n;
         System.out.printf("AVERAGE HEIGHT: %.2f%n", avg);
         sc.close();
