ec4f3caa7ca9f05dbc3598bc62a7a7fc6b8680ac
Leonardo de Camargo <134113848+leo4215@users.noreply.github.com>
2024-01-05 23:02:39 -0500

    Update Program.java

--- Ex14/Program.java
+++ Ex14/Program.java
@@ -1,17 +1,12 @@
-/* A dona de um pensionato possui dez quartos para alugar para estudantes, sendo esses quartos identificados pelos números 0 a 9.
-
-Fazer um programa que inicie com todos os dez quartos vazios, e depois leia uma quantidade N representando o número de estudantes que vão alugar quartos (N pode ser de 1 a 10). Em seguida, registre o aluguel dos N estudantes. Para cada registro de aluguel, informar o nome e email do estudante, bem como qual dos quartos ele escolheu (de 0 a 9). Suponha que seja escolhido um quarto vago. Ao final, seu programa deve imprimir um relatório de todas ocupações do pensionato, por ordem de quarto, conforme exemplo. */
-
 import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        Rent[] vect = new Rent[10];
-      
         System.out.print("How many rooms will be rented? ");
         int n = sc.nextInt();
+        
+        Rent[] vect = new Rent[10];
         
         for (int i = 1; i <= n; i++){
             sc.nextLine();
@@ -31,7 +26,6 @@ public class Program {
                 System.out.println(i + ": " + vect[i]);
             }
         }
-
         sc.close();
     }
 }
